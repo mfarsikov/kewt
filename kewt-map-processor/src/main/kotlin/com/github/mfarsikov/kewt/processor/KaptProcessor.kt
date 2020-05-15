f72a8a866785b1d3e952d35546959f83660c5448
Max Farsikov <farsikovmax@gmail.com>
2020-05-16 00:36:37 +0300

    refactoring

--- kewt-map-processor/src/main/kotlin/com/github/mfarsikov/kewt/processor/KaptProcessor.kt
+++ kewt-map-processor/src/main/kotlin/com/github/mfarsikov/kewt/processor/KaptProcessor.kt
@@ -15,15 +15,15 @@ import com.github.mfarsikov.kewt.processor.render.RenderConverterClass
 import com.github.mfarsikov.kewt.processor.render.RenderConverterFunction
 import com.github.mfarsikov.kewt.processor.render.RenderPropertyMappings
 import com.github.mfarsikov.kewt.processor.render.render
-import com.github.mfarsikov.kewt.processor.render.renderInterface
-import com.github.mfarsikov.kewt.processor.render.renderExt
 import com.github.mfarsikov.kewt.processor.resolver.PropertiesResolver
 import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
 import javax.tools.StandardLocation
 import kotlin.properties.Delegates
@@ -52,7 +52,7 @@ class KewtMapperProcessor : AbstractProcessor() {
 
     override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
         if (roundEnv.processingOver()) return false
-        val now = LocalDateTime.now()
+        val now = LocalDateTime.now().atOffset(ZoneOffset.UTC)
 
         val version = this::class.java.classLoader
                 .getResource("META-INF/build-info.properties")
@@ -66,131 +66,29 @@ class KewtMapperProcessor : AbstractProcessor() {
 
             roundEnv.getElementsAnnotatedWith(Mapper::class.java).forEach { element ->
 
-                val pkg = processingEnv.elementUtils.getPackageOf(element).qualifiedName.toString()
-                if (
-                        whitelist.isNotEmpty() && whitelist.none { pkg.startsWith(it) } || //TODO class level?
-                        blacklist.isNotEmpty() && blacklist.any { pkg.startsWith(it) }
-                ) {
-                    Logger.info("Skip ${pkg}")
-                    return@forEach
-                }
+                if (shouldSkip(element)) return@forEach
 
                 try {
                     val parsedInterface = parse(element, processingEnv)
-
-
-
-
                     Logger.debug("Parsed class: ${parsedInterface.type}")
 
-                    val conversionFunctions = parsedInterface.functions.filter { it.parameters.size == 1 }
-                            .map {
-                                ConversionFunction(
-                                        name = it.name,
-                                        parameter = it.parameters.single().toParameter(),
-                                        returnType = it.returnType,
-                                        isExtension = it.isExtension
-                                )
-                            }
+                    val auxConversionFunctions = parsedInterface.functions.filter { it.parameters.size == 1 }
+                            .map { it.toConversionFunction() }
 
-                    Logger.debug("Conversion functions: $conversionFunctions")
-                    parsedInterface.functions
-                            .filter { !it.abstract && it.parameters.size > 1 }
-                            .map { it.name }
-                            .takeIf { it.isNotEmpty() }
-                            ?.let { Logger.warn("Not abstract function with more than one parameter cannot be used: $it, class: ${parsedInterface.type}") }
+                    Logger.debug("Conversion functions: $auxConversionFunctions")
+                    validate(parsedInterface)
 
                     val mappingsResults = parsedInterface.functions
                             .filter { it.abstract }
-                            .map { parsedFunction ->
+                            .map { abstractConverterFunction -> mapFunction(abstractConverterFunction, propertiesResolver, auxConversionFunctions) }
 
-                                Logger.debug("Abstract function: $parsedFunction")
-                                val nameMappings = normalizeNames(parsedFunction.parameters.map { it.toParameter() }, parsedFunction.annotationConfigs)
-
-
-                                val targetParameter = parsedFunction.parameters.singleOrNull { it.isTarget }
-                                val invalidTargetParameterType = targetParameter?.type?.toSimpleType()
-                                        ?.takeIf { it != parsedFunction.returnType.toSimpleType() }
-
-
-                                if (invalidTargetParameterType != null) throw KewtException("target paratmeter type does not match return type. target parameter: $invalidTargetParameterType, return type:${parsedFunction.returnType.toSimpleType()}")
-
-                                val resolvedSources = parsedFunction.parameters
-                                        .filter { !it.isTarget }
-                                        .map { parameter ->
-
-                                            val ms = nameMappings.filter { it.parameterName == parameter.name }
-
-                                            val nestedSources = propertiesResolver.nestedParameterProperties(ms, parameter.toParameter())
-
-                                            val resolveType = propertiesResolver.resolveType(parameter.type)
-
-                                            val rt = resolveType
-                                                    .mapParameter { p -> Source(parameterName = parameter.name, path = listOf(p.name), type = p.type) }
-                                                    .let {
-                                                        it.copy(properties = it.properties + nestedSources)
-                                                    }
-
-                                            ResolvedParameter(parameter.name, rt)
-                                        }
-
-                                val resolvedReturnType = propertiesResolver.resolveType(parsedFunction.returnType)
-
-                                val returnPropertiesWithDefaultValues = resolvedReturnType.properties.filter { it.hasDefaultValue }.map { it.name }.toSet()
-
-
-                                val sources = resolvedSources.flatMap { it.resolvedType.properties } +
-                                        parsedFunction.parameters.map { Source(it.name, path = emptyList(), type = it.type) }
-
-
-                                val mappings = calculateMappings(
-                                        sources = sources,
-                                        targets = resolvedReturnType.properties.map { Parameter(it.name, it.type) }.toSet(),
-                                        nameMappings = nameMappings,
-                                        explicitConverters = parsedFunction.annotationConfigs
-                                                .filter { it.converter != null }
-                                                .map { ExplicitConverter(targetName = it.target, converterName = it.converter!!) },
-                                        conversionFunctions = conversionFunctions.map { it.toMapperConversionFunction() },
-                                        returnPropertiesWithDefaultValues = returnPropertiesWithDefaultValues
-                                )
-
-                                MappedFunction(
-                                        name = parsedFunction.name,
-                                        parameters = parsedFunction.parameters.map { it.toParameter() },
-                                        returnType = resolvedReturnType.mapParameter { Parameter(it.name, it.type) },
-                                        mappings = mappings,
-                                        targetParameterName = targetParameter?.name
-
-                                )
-                            }
-
-                    val isExtensionByMapperConversionFunction = conversionFunctions.associate { it.toMapperConversionFunction() to it.isExtension }
-                    val renderConverterClass = RenderConverterClass(
-                            isInterface = parsedInterface.isInterface,
-                            version = version,
-                            date = now.atOffset(ZoneOffset.UTC),
-                            type = parsedInterface.type,
-                            converterFunctions = mappingsResults.map {
-                                RenderConverterFunction(
-                                        name = it.name,
-                                        returnTypeLanguage = it.returnType.language,
-                                        parameters = it.parameters,
-                                        returnType = it.returnType.type,
-                                        mappings = it.mappings.map {
-                                            RenderPropertyMappings(
-                                                    parameterName = it.source.parameterName,
-                                                    sourcePropertyName = it.source.path.joinToString("."),//TODO should parser see path as array? for null-safe calls?
-                                                    targetPropertyName = it.target.name,
-                                                    conversionContext = it.conversionContext!!.toRenderConversionContext(isExtensionByMapperConversionFunction[it.conversionContext.conversionFunction])
-                                            )
-                                        },
-                                        targetParameterName = it.targetParameterName
-                                )
-                            },
-                            springComponent = springComponent
-                    )
-
-                    val text = render(renderConverterClass )
+                    val text = render(toRenderConverterClass(
+                            auxConversionFunctions,
+                            parsedInterface,
+                            version,
+                            now,
+                            mappingsResults
+                    ))
 
                     val file = processingEnv.filer.createResource(StandardLocation.SOURCE_OUTPUT, parsedInterface.type.packageName, "${parsedInterface.type.name}Impl.kt", element)
 
@@ -206,6 +104,124 @@ class KewtMapperProcessor : AbstractProcessor() {
         }
 
         return false
+    }
+
+    private fun toRenderConverterClass(
+            auxConversionFunctions: List<ConversionFunction>,
+            parsedInterface: ParsedMapper,
+            version: String,
+            now: OffsetDateTime,
+            mappingsResults: List<MappedFunction>
+    ): RenderConverterClass {
+        val isExtensionByMapperConversionFunction = auxConversionFunctions.associate { it.toMapperConversionFunction() to it.isExtension }
+
+        val renderConverterClass = RenderConverterClass(
+                isInterface = parsedInterface.isInterface,
+                version = version,
+                date = now,
+                type = parsedInterface.type,
+                converterFunctions = mappingsResults.map {
+                    RenderConverterFunction(
+                            name = it.name,
+                            returnTypeLanguage = it.returnType.language,
+                            parameters = it.parameters,
+                            returnType = it.returnType.type,
+                            mappings = it.mappings.map {
+                                RenderPropertyMappings(
+                                        parameterName = it.source.parameterName,
+                                        sourcePropertyName = it.source.path.joinToString("."),//TODO should parser see path as array? for null-safe calls?
+                                        targetPropertyName = it.target.name,
+                                        conversionContext = it.conversionContext!!.toRenderConversionContext(isExtensionByMapperConversionFunction[it.conversionContext.conversionFunction])
+                                )
+                            },
+                            targetParameterName = it.targetParameterName
+                    )
+                },
+                springComponent = springComponent
+        )
+        return renderConverterClass
+    }
+
+    private fun mapFunction(parsedFunction: Function, propertiesResolver: PropertiesResolver, conversionFunctions: List<ConversionFunction>): MappedFunction {
+        Logger.debug("Abstract function: $parsedFunction")
+        val nameMappings = normalizeNames(parsedFunction.parameters.map { it.toParameter() }, parsedFunction.annotationConfigs)
+
+
+        val targetParameter = parsedFunction.parameters.singleOrNull { it.isTarget }
+        val invalidTargetParameterType = targetParameter?.type?.toSimpleType()
+                ?.takeIf { it != parsedFunction.returnType.toSimpleType() }
+
+
+        if (invalidTargetParameterType != null) throw KewtException("target paratmeter type does not match return type. target parameter: $invalidTargetParameterType, return type:${parsedFunction.returnType.toSimpleType()}")
+
+        val resolvedSources = parsedFunction.parameters
+                .filter { !it.isTarget }
+                .map { parameter ->
+
+                    val ms = nameMappings.filter { it.parameterName == parameter.name }
+
+                    val nestedSources = propertiesResolver.nestedParameterProperties(ms, parameter.toParameter())
+
+                    val resolveType = propertiesResolver.resolveType(parameter.type)
+
+                    val rt = resolveType
+                            .mapParameter { p -> Source(parameterName = parameter.name, path = listOf(p.name), type = p.type) }
+                            .let {
+                                it.copy(properties = it.properties + nestedSources)
+                            }
+
+                    ResolvedParameter(parameter.name, rt)
+                }
+
+        val resolvedReturnType = propertiesResolver.resolveType(parsedFunction.returnType)
+
+        val returnPropertiesWithDefaultValues = resolvedReturnType.properties.filter { it.hasDefaultValue }.map { it.name }.toSet()
+
+
+        val sources = resolvedSources.flatMap { it.resolvedType.properties } +
+                parsedFunction.parameters.map { Source(it.name, path = emptyList(), type = it.type) }
+
+
+        val mappings = calculateMappings(
+                sources = sources,
+                targets = resolvedReturnType.properties.map { Parameter(it.name, it.type) }.toSet(),
+                nameMappings = nameMappings,
+                explicitConverters = parsedFunction.annotationConfigs
+                        .filter { it.converter != null }
+                        .map { ExplicitConverter(targetName = it.target, converterName = it.converter!!) },
+                conversionFunctions = conversionFunctions.map { it.toMapperConversionFunction() },
+                returnPropertiesWithDefaultValues = returnPropertiesWithDefaultValues
+        )
+
+        return MappedFunction(
+                name = parsedFunction.name,
+                parameters = parsedFunction.parameters.map { it.toParameter() },
+                returnType = resolvedReturnType.mapParameter { Parameter(it.name, it.type) },
+                mappings = mappings,
+                targetParameterName = targetParameter?.name
+
+        )
+    }
+
+    private fun validate(parsedInterface: ParsedMapper) {
+        parsedInterface.functions
+                .filter { !it.abstract && it.parameters.size > 1 }
+                .map { it.name }
+                .takeIf { it.isNotEmpty() }
+                ?.let { Logger.warn("Not abstract function with more than one parameter cannot be used: $it, class: ${parsedInterface.type}") }
+    }
+
+    private fun shouldSkip(element: Element): Boolean {
+        val pkg = processingEnv.elementUtils.getPackageOf(element).qualifiedName.toString()
+        return if (
+                whitelist.isNotEmpty() && whitelist.none { pkg.startsWith(it) } || //TODO class level?
+                blacklist.isNotEmpty() && blacklist.any { pkg.startsWith(it) }
+        ) {
+            Logger.info("Skip ${pkg}")
+            true
+        } else {
+            false
+        }
     }
 
     private fun normalizeNames(
@@ -254,11 +270,13 @@ data class ConversionFunction(
 ) {
     override fun toString() = "$name($parameter): $returnType"
 }
+
 fun ConversionFunction.toMapperConversionFunction() = MapperConversionFunction(
         name = name,
         parameter = parameter,
         returnType = returnType
 )
+
 fun MappingConversionContext.toRenderConversionContext(isExtension: Boolean?) = RenderConversionContext(
         conversionFunction = conversionFunction?.toConversionFunction(isExtension!!),
         usingElementMapping = usingElementMapping,
@@ -269,6 +287,13 @@ fun MappingConversionContext.toRenderConversionContext(isExtension: Boolean?) = 
 fun MapperConversionFunction.toConversionFunction(isExtension: Boolean) = ConversionFunction(
         name = name,
         parameter = parameter,
+        returnType = returnType,
+        isExtension = isExtension
+)
+
+fun Function.toConversionFunction() = ConversionFunction(
+        name = name,
+        parameter = parameters.single().toParameter(),
         returnType = returnType,
         isExtension = isExtension
 )
