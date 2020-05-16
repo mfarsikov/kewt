dbef6a5be1673e611a73dabb39a59d2bccf43e88
Max Farsikov <farsikovmax@gmail.com>
2020-05-16 20:30:45 +0300

    version config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-plugins{
+import com.github.mfarsikov.kewt.versioning.plugin.BranchConfig
+import java.time.ZoneId
+
+plugins {
     kotlin("jvm") version "1.3.71" apply false
     id("com.jfrog.bintray") version "1.8.5" apply false
     id("com.github.mfarsikov.kewt-versioning") version "0.4.0-dirty"
@@ -7,6 +10,18 @@ plugins{
 repositories {
     mavenLocal()
     mavenCentral()
+}
+kewtVersioning {
+    branches = mutableListOf(
+            BranchConfig().apply {
+                regexes = mutableListOf("master".toRegex())
+                stringify = smartVersionStringifier(useBranch = false)
+            },
+            BranchConfig().apply {
+                regexes = mutableListOf(".*".toRegex())
+                stringify = smartVersionStringifier(useTimestamp = true, zoneId = ZoneId.of("Europe/Kiev"))
+            }
+    )
 }
 val v = kewtVersioning.version
 subprojects {
