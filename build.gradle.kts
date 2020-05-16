96a28efac519a00a03bef1f8d012842e0a4591f8
Max Farsikov <farsikovmax@gmail.com>
2020-05-16 12:07:37 +0300

    test versioning plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins{
     kotlin("jvm") version "1.3.71" apply false
     id("com.jfrog.bintray") version "1.8.5" apply false
-    id("com.github.mfarsikov.kewt-versioning") version "0.3.0"
+    id("com.github.mfarsikov.kewt-versioning") version "0.4.0-dirty"
 }
 
 repositories {
