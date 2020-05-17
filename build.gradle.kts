5511ac15b47f810eabb3f370bf4d1a575870cb5a
Max Farsikov <farsikovmax@gmail.com>
2020-05-17 21:21:20 +0300

    upgrade gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,16 @@
-import com.github.mfarsikov.kewt.versioning.plugin.BranchConfig
-import java.time.ZoneId
-
 plugins {
     kotlin("jvm") version "1.3.71" apply false
     id("com.jfrog.bintray") version "1.8.5" apply false
-    id("com.github.mfarsikov.kewt-versioning") version "0.4.0-dirty"
+    id("com.github.mfarsikov.kewt-versioning") version "0.6.0"
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
 }
-kewtVersioning {
-    branches = mutableListOf(
-            BranchConfig().apply {
-                regexes = mutableListOf("master".toRegex())
-                stringify = smartVersionStringifier(useBranch = false)
-            },
-            BranchConfig().apply {
-                regexes = mutableListOf(".*".toRegex())
-                stringify = smartVersionStringifier(useTimestamp = true, zoneId = ZoneId.of("Europe/Kiev"))
-            }
-    )
-}
+
 val v = kewtVersioning.version
+
 subprojects {
     version = v
 }
